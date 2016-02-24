11e8bccc4a68b8d78828feb9bc2616036ea3c1f6
Elomar Souza <elomar@elomar.me>
2016-02-23 23:55:15 -0800

    Add parser for 37signals Manifesto

---
+++ lib/parsers/manifesto.rb
@@ -0,0 +1,37 @@
+require 'mechanize'
+
+module Parsers
+  class Manifesto
+    INDEX = "https://37signals.com/manifesto"
+    SOURCE = "37signals Manifesto"
+
+    def parse(&callback)
+      urls.each do |url|
+        callback.call(document(url))
+      end
+    end
+
+    private
+
+    def urls
+      agent.get(INDEX).search('//li/a/@href').map(&:value)
+    end
+
+    def document(url)
+      page = agent.get(url)
+
+      return { 
+        title: page.search('h1').text,
+        content: page.search('span.body').first.text.strip,
+        author: nil,
+        source: SOURCE,
+        url: url,
+        published_on: nil
+      }
+    end
+
+    def agent
+      @agent ||= Mechanize.new
+    end
+  end
+end
